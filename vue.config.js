4f546d076605287b3736c3727ab9b7498637a185
yar9bit <yar9bit@gmail.com>
2022-07-12 20:25:40 +0500

    Update anim

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,5 @@ module.exports = {
   devServer: {
     disableHostCheck: true
   },
-  publicPath: process.env.NODE_ENV === "/<REPO>/"
-    ? '/ZNote/'
-    : '/'
-}
+   publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
+};
