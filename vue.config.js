f9fcc707768ad9c24044d4f1a512e83c8e9b6061
yar9bit <yar9bit@gmail.com>
2022-07-12 18:12:55 +0500

    Update

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devServer: {
     disableHostCheck: true
   },
-  publicPath: process.env.NODE_ENV === 'production'
+  publicPath: process.env.NODE_ENV === "/<REPO>/"
     ? '/ZNote/'
     : '/'
 }
